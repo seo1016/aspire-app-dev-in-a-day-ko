289bddf569da12d08740caa668a69b47f1f5e835
seo1016 <seoseunghun3@gmail.com>
2024-08-01 00:27:55 +0900

    스웨거 드가자

--- workshop/AspireYouTubeSummariser.ApiApp/Program.cs
+++ workshop/AspireYouTubeSummariser.ApiApp/Program.cs
@@ -36,11 +36,8 @@ var app = builder.Build();
 app.MapDefaultEndpoints();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
